b605eb8ced4d6973f2b9a385438500412489781f
Jefferson <jeffersonguerrero415@gmail.com>
2026-05-04 18:30:44 -0500

    feat: usar control flow moderno en profile page

--- src/app/features/profile/pages/profile-page.ts
+++ src/app/features/profile/pages/profile-page.ts
@@ -10,11 +10,23 @@ export class ProfilePage {
   readonly lastName = signal('Pérez');
   readonly age = signal(30);
 
+  readonly skills = signal(['Angular', 'TypeScript', 'HTML']);
+
   readonly fullName = computed(() => `${this.firstName()} ${this.lastName()}`);
+
+  readonly ageCategory = computed(() => {
+    const value = this.age();
+
+    if (value < 18) return 'minor';
+    if (value < 30) return 'young';
+    if (value < 60) return 'adult';
+    return 'senior';
+  });
 
   changeData() {
     this.firstName.set('Ana');
     this.lastName.set('González');
     this.age.set(22);
+    this.skills.set(['Angular', 'RxJS', 'SCSS']);
   }
 }
